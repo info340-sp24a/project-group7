38387b0a9a3b874b3a7da7cfec5f8421f8f376e0
lyrissefaith <lyrissefaith@gmail.com>
2024-05-15 20:19:52 -0700

    fixed title alignment

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,28 +5,23 @@ import '../css/main.css'; // Import the CSS file
 const Navbar = () => {
     return (
         <nav className="navbar">
-            {/* Menu for smaller screens */}
-            <div className="hamburger-btn" aria-label="Menu">
-                <i className="fas fa-bars"></i>
-            </div>
-            {/* Pages */}
-            <div className="pages">
+            {/*Pages */}
+            <div class="pages">
                 <ul>
-                    <li><a href="index.html" aria-current="page">Home</a></li>
-                    <li><a href="calendar.html">Calendar</a></li>
-                    <li><a href="diaries.html">Diary</a></li>
-                    <li><a href="info.html">Plant Info</a></li>
+                    <li><a href="./index.html">Home</a></li>
+                    <li><a href="./calendar.html">Calendar</a></li>
+                    <li><a href="./diaries.html">Diary</a></li>
+                    <li><a href="./Info.html">Plant Info </a></li>
                 </ul>
             </div>
-            {/* Title */}
-            <header>
-                <h1>Plant Parenthood</h1>
-            </header>
-            {/* Profile */}
-            <div className="profile">
+
+            {/*Title */}
+            <h1>Plant Parenthood</h1>
+            {/*Profile */}
+            <div class="profile">
                 <ul>
-                    <li><a href="login.html">Profile</a></li>
-                    <li><a href="signup.html">Sign Up</a></li>
+                    <li><a href="./login.html">Profile</a></li>
+                    <li><a href="./login.html">Sign Up</a></li>
                 </ul>
             </div>
         </nav>
