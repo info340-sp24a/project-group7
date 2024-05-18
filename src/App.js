bf3105c7cabaf6e74e064cf79058b456616982b2
lyrissefaith <lyrissefaith@gmail.com>
2024-05-17 20:20:16 -0700

    comments for file clarity

--- src/App.js
+++ src/App.js
@@ -2,19 +2,23 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import SideNav from './components/Sidenav';
 import PlantGallery from './components/mainindex';
-import Plant from './components/plantinfo'; 
-import Calendar from './components/calendar';
-
+//import Plant from './components/plantinfo'; 
+//import Calendar from './components/calendar';
 //import SideBox from './components/SideBox';
 
 
 const App = () => {
     return (
         <div className="app-container">
+            {/* Main Navigation Bar */}
             <Navbar />
+            {/* Media Query Navigation Bar */}
             <SideNav />
+            {/* Index Page */}
             <PlantGallery />
+            {/* Plant Biography Page */}
             {/*<Plant/>*/}
+            {/* Calender Page */}
             {/*<Calendar />*/ }
             
             <footer>
