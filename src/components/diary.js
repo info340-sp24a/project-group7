f2f44ce5ede9b7204f0bb84562b3352f1d9c15a3
lyrissefaith <lyrissefaith@gmail.com>
2024-06-02 16:00:22 -0700

    interactivity for calendar

--- src/components/diary.js
+++ src/components/diary.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import '../css/diary.css';
+import '../css/main.css';
 
 const Diary = () => {
   const [title, setTitle] = useState('');
