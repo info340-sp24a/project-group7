635601dfbf5587ae7e79933116ad6ae5e787dc95
lyrissefaith <lyrissefaith@gmail.com>
2024-06-03 18:36:07 -0700

    user must login to access calendar and diary

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React, { useState } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar'; // Ensure correct import path
 import PlantGallery from './components/index';
 import Plant from './components/plantinfo';
 import Calendar from './components/calendar';
 import Diary from './components/diary';
 import Signin from './components/signin';
-import SearchFilter from './components/SearchFilter'; // Import SearchFilter component
-import plantData from './data/plantData';
+import plantData  from './data/plantData';
+import { getAuth } from 'firebase/auth';
+import { useAuthState } from 'react-firebase-hooks/auth';
 
 function App() {
+    const auth = getAuth();
+    const [user, loading, error] = useAuthState(auth);
     const [filteredPlants, setFilteredPlants] = useState(plantData);
 
     const handleSearch = (searchTerm) => {
@@ -19,16 +22,23 @@ function App() {
         setFilteredPlants(filtered);
     };
 
+    if (loading) {
+        return <p>Initializing user...</p>;
+    }
+
+    if (error) {
+        return <p>Error: {error.message}</p>;
+    }
+
     return (
         <>
-            {/* Main Navigation Bar */}
             <Navbar />
 
             <Routes>
-                <Route path="/" element={<PlantGallery />} />
+                <Route path="/" element={<PlantGallery onSearch={handleSearch} filteredPlants={filteredPlants} setFilteredPlants={setFilteredPlants} />} />
                 <Route path="/plantbio" element={<Plant />} />
-                <Route path="/calendar" element={<Calendar />} />
-                <Route path="/diary" element={<Diary />} />
+                <Route path="/calendar" element={user ? <Calendar /> : <Navigate to="/signin" />} />
+                <Route path="/diary" element={user ? <Diary /> : <Navigate to="/signin" />} />
                 <Route path="/signin" element={<Signin />} />
             </Routes>
 
